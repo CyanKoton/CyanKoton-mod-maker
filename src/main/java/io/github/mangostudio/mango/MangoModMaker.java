39032942281da6fe5a82670486863d1c69307fa1
beanflame <68175497+beanflame@users.noreply.github.com>
2021-09-12 18:11:09 +0800

    Update MangoModMaker.java

--- src/main/java/io/github/mangostudio/mango/MangoModMaker.java
+++ src/main/java/io/github/mangostudio/mango/MangoModMaker.java
@@ -6,5 +6,6 @@ public class MangoModMaker {
     public static final String VERSION = "0.1.0-Alpha";
     public static final String DONATE_URL = "https://afdian.net/@beanflame";
     public static final String GITEE_URL = "https://gitee.com/mango-studio/mango-mod-maker";
+    public static final String GITHUB_URL = "https://github.com/mango-studio/mango-mod-maker";
     public static final String UPDATE_LOG_URL = "https://shimo.im/docs/DtCrHYxvHCdGHrhq/";
 }
